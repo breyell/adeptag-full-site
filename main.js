ed6ff48c3206fee76b8f1615abc0aa6ceccbf068
Brendon Reyell <brendon@reyell.net>
2026-01-20 13:38:20 -0600

    product filter updates

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-import Alpine from 'alpinejs'
-import focus from '@alpinejs/focus'
-// import resize from '@alpinejs/resize'
-import collapse from '@alpinejs/collapse'
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
+import resize from "@alpinejs/resize";
+import collapse from "@alpinejs/collapse";
 
-import autoAnimate from '@formkit/auto-animate'
+import autoAnimate from "@formkit/auto-animate";
 
-import plyr from './resources/js/alpine/plyr.js'
-import blogFilter from './resources/js/alpine/blog-filter.js'
-import newsFilter from './resources/js/alpine/news-filter.js'
-import eventFilter from './resources/js/alpine/event-filter.js'
-import pressFilter from './resources/js/alpine/press-filter.js'
-import storyFilter from './resources/js/alpine/story-filter.js'
-import productFilter from './resources/js/alpine/product-filter.js'
-import testimonialCarousel from './resources/js/alpine/testimonial-carousel.js'
-import productHeroCarousel from './resources/js/alpine/product-hero-carousel.js'
-import successStoriesCarousel from './resources/js/alpine/success-stories-carousel.js'
-import productHeroCarouselVideo from './resources/js/alpine/product-hero-carousel-video.js'
-import successStoriesCarouselVideo from './resources/js/alpine/success-stories-carousel-video.js'
+import plyr from "./resources/js/alpine/plyr.js";
+import blogFilter from "./resources/js/alpine/blog-filter.js";
+import newsFilter from "./resources/js/alpine/news-filter.js";
+import eventFilter from "./resources/js/alpine/event-filter.js";
+import pressFilter from "./resources/js/alpine/press-filter.js";
+import storyFilter from "./resources/js/alpine/story-filter.js";
+import productFilter from "./resources/js/alpine/product-filter.js";
+import testimonialCarousel from "./resources/js/alpine/testimonial-carousel.js";
+import productHeroCarousel from "./resources/js/alpine/product-hero-carousel.js";
+import successStoriesCarousel from "./resources/js/alpine/success-stories-carousel.js";
+import productHeroCarouselVideo from "./resources/js/alpine/product-hero-carousel-video.js";
+import successStoriesCarouselVideo from "./resources/js/alpine/success-stories-carousel-video.js";
 
-Alpine.plugin([focus, collapse])
+Alpine.plugin([focus, resize, collapse]);
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
-Alpine.data('plyr', plyr)
-Alpine.data('blogFilter', blogFilter)
-Alpine.data('newsFilter', newsFilter)
-Alpine.data('eventFilter', eventFilter)
-Alpine.data('pressFilter', pressFilter)
-Alpine.data('storyFilter', storyFilter)
-Alpine.data('productFilter', productFilter)
-Alpine.data('testimonialCarousel', testimonialCarousel)
-Alpine.data('productHeroCarousel', productHeroCarousel)
-Alpine.data('successStoriesCarousel', successStoriesCarousel)
-Alpine.data('productHeroCarouselVideo', productHeroCarouselVideo)
-Alpine.data('successStoriesCarouselVideo', successStoriesCarouselVideo)
+Alpine.data("plyr", plyr);
+Alpine.data("blogFilter", blogFilter);
+Alpine.data("newsFilter", newsFilter);
+Alpine.data("eventFilter", eventFilter);
+Alpine.data("pressFilter", pressFilter);
+Alpine.data("storyFilter", storyFilter);
+Alpine.data("productFilter", productFilter);
+Alpine.data("testimonialCarousel", testimonialCarousel);
+Alpine.data("productHeroCarousel", productHeroCarousel);
+Alpine.data("successStoriesCarousel", successStoriesCarousel);
+Alpine.data("productHeroCarouselVideo", productHeroCarouselVideo);
+Alpine.data("successStoriesCarouselVideo", successStoriesCarouselVideo);
 
-Alpine.directive('auto-animate', (el, { expression }, { evaluate }) => {
-	autoAnimate(el, (expression && evaluate(expression)) || {})
-})
+Alpine.directive("auto-animate", (el, { expression }, { evaluate }) => {
+	autoAnimate(el, (expression && evaluate(expression)) || {});
+});
 
-Alpine.start()
+Alpine.start();
--- resources/js/alpine/product-filter.js
+++ resources/js/alpine/product-filter.js
@@ -1,31 +1,46 @@
 export default function ($products) {
 	return {
-		showFilters: true,
+		showFilters: false,
 		products: $products,
 		selectedMarkets: [],
 		currentPage: 1,
 		perPage: 12,
+		width: undefined,
+		init() {
+			this.$watch("width", () => {
+				this.perPage = this.width < 768 ? 4 : 12;
+				if (this.width < 370) {
+					this.perPage = 4;
+				} else if (this.width < 1024) {
+					this.perPage = 6;
+				} else if (this.width < 1280) {
+					this.perPage = 9;
+				} else {
+					this.perPage = 12;
+				}
+			});
+		},
 		get totalPages() {
-			return Math.ceil(this.filteredProducts.length / this.perPage)
+			return Math.ceil(this.filteredProducts.length / this.perPage);
 		},
 		get filteredProducts() {
 			let filteredProducts = this.products;
 
 			if (this.selectedMarkets.length) {
-				const selectedMarketsSet = new Set(this.selectedMarkets)
+				const selectedMarketsSet = new Set(this.selectedMarkets);
 				filteredProducts = filteredProducts.filter((product) => {
-					const set1 = new Set(product.markets)
-					return !!set1.intersection(selectedMarketsSet).size
-				})
+					const set1 = new Set(product.markets);
+					return !!set1.intersection(selectedMarketsSet).size;
+				});
 			}
 
-			return filteredProducts
+			return filteredProducts;
 		},
 		get displayedProducts() {
 			return this.filteredProducts.slice(
 				(this.currentPage - 1) * this.perPage,
-				this.currentPage * this.perPage
-			)
+				this.currentPage * this.perPage,
+			);
 		},
-	}
+	};
 }
